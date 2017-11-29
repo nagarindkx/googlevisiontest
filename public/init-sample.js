a50b774f5ddd6f06f1beee0187bacd0c824b6085
Kanakorn Horsiritham <kanakorn.h@psu.ac.th>
2017-11-29 13:21:19 +0700

    Initial Commit

---
+++ public/init-sample.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "<your-api-key>",
+    authDomain: "<your-auth-domain>",
+    databaseURL: "<your-database-url>",
+    projectId: "<your-project-id>",
+    storageBucket: "<your-storage-bucket>",
+    messagingSenderId: "<your-messaging-sender-id>"
+  };
+  firebase.initializeApp(config);
