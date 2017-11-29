c012af224c8402461a0edc688e6bccdb0639f773
Kanakorn Horsiritham <kanakorn.h@psu.ac.th>
2017-11-29 17:00:06 +0700

    Add to user's images folder

---
+++ public/storage.js
@@ -0,0 +1,29 @@
+var uploader = document.getElementById("uploader");
+var fileButton = document.getElementById("fileButton");
+var storageRef;
+firebase.auth().onAuthStateChanged(function(user) {
+    storageRef = firebase.storage().ref('images/' + user.uid + '/target.png');
+});
+
+fileButton.addEventListener('change', function(e){
+    //console.log(">>>" + e.target.files[0].name);
+    var file = e.target.files[0];
+    
+    var task = storageRef.put(file);
+
+    task.on('state_changed',
+            function progress(snapshot){
+                var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                uploader.value = percentage;
+            },
+            null,
+            function complete(){                                
+                console.log('upload completed ' + storageRef.fullPath );     
+                storageRef.getDownloadURL().then(function(url){
+                    document.getElementById("targetImage").src = url;
+                });                     
+            }
+    );
+
+
+}); 
