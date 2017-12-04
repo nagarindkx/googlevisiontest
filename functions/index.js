6353410bb9412c49443f58fc1f8a632ed7cd3e4d
kanakorn <kanakorn@ws13.cc.psu.ac.th>
2017-12-04 15:48:30 +0700

    Final myVision Demo

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,12 @@
-//import { firestore } from 'firebase-admin';
-
 const functions = require('firebase-functions');
-// const testvision    = require("./vision");
+const allowOrigin = "https://" + functions.config().firebase.projectId + ".firebaseapp.com";
 
-// exports.myvision = functions.https.onRequest((req,resp) => {
-//     testvision.handler("gs://visiontest-fb8ef.appspot.com/images/vNXSW9fTfmfG05Nyp1GbRFWpvtM2/target.png",resp);
-// })
-
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-exports.sampleFunction = functions.https.onRequest((req,resp)=> {
-    response.setHeader('Access-Control-Allow-Origin', config.authDomain);
-    resp.status(200).send("<h1>Hello</h1>");
+exports.sampleFunction = functions.https.onRequest((request,response)=> {
+    response.setHeader('Access-Control-Allow-Origin', allowOrigin );
+    response.status(200).send("<h1>Hello World</h1>");
 });
 
-exports.myvision = functions.https.onRequest((request, response) => {
+exports.myVision = functions.https.onRequest((request, response) => {
     // Imports the Google Cloud client library
      const vision = require('@google-cloud/vision');
      const client = new vision.ImageAnnotatorClient();
@@ -32,7 +18,7 @@ exports.myvision = functions.https.onRequest((request, response) => {
         labels.forEach(function(label){
             txt = txt + "</br>" + label.description;
             });
-        response.setHeader('Access-Control-Allow-Origin', config.authDomain);
+        response.setHeader('Access-Control-Allow-Origin', allowOrigin);
         response.send(txt);                
         }).catch((error)=>{txt=error});
     }) ;
